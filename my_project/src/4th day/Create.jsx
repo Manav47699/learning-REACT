da417f23ee4ac92e119a884d3cd72114d1700ea1
Manav47699 <acharyamanav7@gmail.com>
2025-07-21 18:43:53 +0545

    IDK tenth maybe

--- my_project/src/4th day/Create.jsx
+++ my_project/src/4th day/Create.jsx
@@ -1,13 +1,94 @@
-import Navbar from "./component/Navbar";
+import { useState } from "react"
+import Navbar from "./component/Navbar"
+import axios from "axios"
+import { useNavigate, useParams } from "react-router-dom";
 
-function Create()
-{
+//for user input in jsx, use onChange={(event)=> {function(event.target.value)}} 
+//on changeing, event (i.e is current character by the user) with target the value={}(that we pass just 1 line before)
+
+function Create() {
+    const [title, setTitle] = useState("")
+    const [subtitle, setsubtitle] = useState("")
+    const [description, setdescription] = useState("")
+    const [image, setimage] = useState("")
+    const navigate = useNavigate();
+
+    
+   
+
+
+//     async function sentdata(e)
+//     {
+// //yo tala ko line lekhnai parxa hai bro
+//         e.preventDefault()
+
+// // axios post must be in object format as our database is in JSON. look at the api to understand
+//         const response = await axios.post("https://687af359abb83744b7ee46ad.mockapi.io/blogs"),
+//         {
+//                 title : title, subtitle:subtitle, description: description, image:image
+//             }
+    //     }
+    
+    async function sendDataToBackend(e) {
+        e.preventDefault()
+        const response = await axios.post("https://687af359abb83744b7ee46ad.mockapi.io/blogs", {
+            title:title,
+            subtitle:subtitle,
+            description:description,
+            image:image,
+        })
+        if (response.status == 201)
+        {
+            navigate("/")
+        }
+        console.log(response)
+        
+    }
+
     return (
         <>
             <Navbar />
-            <h1 className="text-center text-8xl">This is create page</h1>
+            <form onSubmit={sendDataToBackend}>
+                <h1>ENTER NAME</h1>
+
+            <input type="text" placeholder="ENter the name" value={title}
+                onChange={(event) => {
+                    setTitle(event.target.value)
+                }
+                }/>
+                
+            
+                        <h1>ENter subtitle</h1>
+            <input type="text" placeholder="ENter the new subtitle" value={subtitle}
+                onChange={(event) => {
+                    setsubtitle(event.target.value)
+                }
+                    
+                    }/>
+                
+            
+                        <h1>ENTER description</h1>
+            <input type="text" placeholder="ENter the name" value={description}
+                onChange={(event) => {
+                    setdescription(event.target.value)
+                }
+                }/>
+                
+            
+                        <h1>ENTER IMage url</h1>
+            <input type="text" placeholder="ENter the name" value={image}
+                onChange={(event) => {
+                    setimage(event.target.value)
+                }
+                }/>
+
+                
+                button must have a type="submit"
+                
+                <button type="submit" className="bg-blue-500 hover:bg-amber-300 rounded-2xl">OK</button>
+            </form>
+
         </>
-        
     )
 }
 
--- my_project/src/4th day/Edit_blog.jsx
+++ my_project/src/4th day/Edit_blog.jsx
@@ -1,60 +1,96 @@
-import { use } from "react"
+//for user input in jsx, use onChange={(event)=> {function(event.target.value)}}
+//on changeing, event (i.e is current character by the user) with target the value={}(that we pass just 1 line before)
+
 import { useState } from "react"
+import Navbar from "./component/Navbar"
+import axios from "axios"
+import { useParams } from "react-router-dom"
 
 //for user input in jsx, use onChange={(event)=> {function(event.target.value)}} 
 //on changeing, event (i.e is current character by the user) with target the value={}(that we pass just 1 line before)
 
-function Edit_blog()
-{
-    const [name, setname] = useState("")
-    const [subtitle, setsubtitle] = useState()
-    const [description, setdescription] = useState()
+function Edit_blog() {
+    const [title, setTitle] = useState("")
+    const [subtitle, setsubtitle] = useState("")
+    const [description, setdescription] = useState("")
+    const [image, setimage] = useState("")
+    const data = useParams()
+
+
+
+    async function sendeditdata(e)
+    {
+        e.preventDefault()
+        const response = await axios.put("https://687af359abb83744b7ee46ad.mockapi.io/blogs/" + data.id, {
+            title: title, 
+            subtitle: subtitle, 
+            description: description,
+            image:image,
+        })
+        console.log(response)
+
+
+    }
+   
+
+
+//     async function sentdata(e)
+//     {
+// //yo tala ko line lekhnai parxa hai bro
+//         e.preventDefault()
+
+// // axios post must be in object format as our database is in JSON. look at the api to understand
+//         const response = await axios.post("https://687af359abb83744b7ee46ad.mockapi.io/blogs"),
+//         {
+//                 title : title, subtitle:subtitle, description: description, image:image
+//             }
+    //     }
+    
+   
 
     return (
         <>
-            <div>
-                <h1>EDIT NAME</h1>
-            <input type="text" placeholder="ENter the name" value={name}
+            <Navbar />
+            <form onSubmit={sendeditdata}>
+                <h1>ENTER NAME</h1>
+
+            <input type="text" placeholder="ENter the name" value={title}
                 onChange={(event) => {
-                    setname(event.target.value)
+                    setTitle(event.target.value)
                 }
-                }></input>
-                <button className="bg-blue-500 hover:bg-amber-300 rounded-4xl">OK</button>
-            </div>
-
-            <div>
-            <h1>EDIT NAME</h1>
-            <input type="text" placeholder="ENter the name" value={name}
-                onChange={(event) => {
-                    setname(event.target.value)
-                }
-                }></input>
-                <button className="bg-blue-500 hover:bg-amber-300 rounded-4xl">OK</button>
-            </div>
-
-
-
-
-
-            <div>
-                        <h1>EDIT subtitle</h1>
+                }/>
+                
+            
+                        <h1>ENter subtitle</h1>
             <input type="text" placeholder="ENter the new subtitle" value={subtitle}
                 onChange={(event) => {
                     setsubtitle(event.target.value)
                 }
-                }></input>
+                    
+                    }/>
+                
             
-                        <h1>EDIT description</h1>
+                        <h1>ENTER description</h1>
             <input type="text" placeholder="ENter the name" value={description}
                 onChange={(event) => {
                     setdescription(event.target.value)
                 }
-                }></input>
-                <button className="bg-blue-500 hover:bg-amber-300 rounded-2xl">OK</button>
-            </div>
-
+                }/>
+                
             
-        
+                        <h1>ENTER IMage url</h1>
+            <input type="text" placeholder="ENter the name" value={image}
+                onChange={(event) => {
+                    setimage(event.target.value)
+                }
+                }/>
+
+                
+                button must have a type="submit"
+                
+                <button type="submit" className="bg-blue-500 hover:bg-amber-300 rounded-2xl">OK</button>
+            </form>
+
         </>
     )
 }
